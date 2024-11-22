27649845c0b58a52ce5bb61973f15225cd2ab2b1
Ahmet Yusuf Yildirim <140295077+AyYildirim-05@users.noreply.github.com>
2024-11-21 19:52:13 -0500

    post

--- src/main/java/edu/vanier/spaceshooter/controllers/SpaceShooterAppController.java
+++ src/main/java/edu/vanier/spaceshooter/controllers/SpaceShooterAppController.java
@@ -7,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
